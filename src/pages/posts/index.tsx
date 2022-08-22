ded8dcd73e04397c609413dcb4f7780c7ce6ecce
ygorperez <azaz22155@gmail.com>
2022-08-22 06:23:49 -0300

    crud

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,25 +1,31 @@
-import Link from 'next/link'
-import { trpc } from '../../utils/trpc'
+import Link from "next/link";
+import { trpc } from "../../utils/trpc";
 
 function PostListingPage() {
-  const { data, isLoading } = trpc.useQuery(['posts.posts'])
+  const { data, isLoading } = trpc.useQuery(["posts.posts"]);
+
+  interface Post {
+    id: string;
+    title: string;
+    body?: string;
+  }
 
   if (isLoading) {
-    return <p>Loading...</p>
+    return <p>Loading...</p>;
   }
 
   return (
     <div>
-      {data?.map((post) => {
+      {data?.map((post: Post) => {
         return (
           <article key={post.id}>
             <p>{post.title}</p>
             <Link href={`/posts/${post.id}`}>Read post</Link>
           </article>
-        )
+        );
       })}
     </div>
-  )
+  );
 }
 
-export default PostListingPage
+export default PostListingPage;
