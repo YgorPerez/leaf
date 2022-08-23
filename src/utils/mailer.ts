02256e38aef834902a74ea4ab535fe6d7175fb71
Ygor Perez de Oliveira <azaz22155@gmail.com>
2022-08-23 15:22:29 -0300

    update

--- src/utils/mailer.ts
+++ src/utils/mailer.ts
@@ -19,6 +19,14 @@ export async function sendLoginEmail({
     },
   });
 
+  transporter.verify(function (error, success) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Server is ready to take our messages");
+    }
+  });
+
   const info = await transporter.sendMail({
     from: `"Equipe Leaf" ${process.env.EMAIL_ADDRESS}`,
     to: email,
