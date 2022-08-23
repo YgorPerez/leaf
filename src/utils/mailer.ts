3440420b9bff0044007fc4d2828fe2266726149b
Ygor Perez de Oliveira <azaz22155@gmail.com>
2022-08-23 04:50:06 -0300

    update

--- src/utils/mailer.ts
+++ src/utils/mailer.ts
@@ -1,5 +1,4 @@
 import nodemailer from "nodemailer";
-import { smtpConfig } from "./smtp";
 
 export async function sendLoginEmail({
   email,
@@ -11,17 +10,17 @@ export async function sendLoginEmail({
   token: string;
 }) {
   const transporter = nodemailer.createTransport({
-    host: smtpConfig.host,
-    port: smtpConfig.port,
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
     secure: false,
     auth: {
-      user: smtpConfig.user,
-      pass: smtpConfig.pass,
+      user: process.env.EMAIL_ADDRESS,
+      pass: process.env.EMAIL_PASS,
     },
   });
 
   const info = await transporter.sendMail({
-    from: `"Leaf Team" ${process.env.SENDER_EMAIL_ADDRESS}`,
+    from: `"Equipe Leaf" ${process.env.EMAIL_ADDRESS}`,
     to: email,
     subject: "Faça o login na sua conta",
     html: `Faça o login na sua conta clicando neste link <a href="${url}/login#token=${token}">Aqui</a>`,
--- src/utils/smtp.ts
+++
@@ -1,6 +0,0 @@
-export const smtpConfig = {
-  host: process.env.EMAIL_HOST,
-  port: process.env.EMAIL_PORT,
-  user: process.env.EMAIL_ADDRESS,
-  pass: process.env.EMAIL_PASS,
-};
